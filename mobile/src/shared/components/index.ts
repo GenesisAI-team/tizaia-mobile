b9dd9c76aada0d584acca0ac4df0ff62d9807287
salinas116030 <salinas.116030@gmail.com>
2026-08-12 14:31:31 +0200

    feat(attendance): add visual matrix design

--- mobile/src/shared/components/index.ts
+++ mobile/src/shared/components/index.ts
@@ -1,4 +1,6 @@
 export { ActionIconButton } from './ActionIconButton';
+export { MatrixBoard } from './MatrixBoard';
+export type { MatrixBoardColumn, MatrixBoardRow } from './MatrixBoard';
 export { ScreenTitle } from './ScreenTitle';
 export { StatusCircleButton } from './StatusCircleButton';
 export { StudentAvatar } from './StudentAvatar';
